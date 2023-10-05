7e91fc0e768a9dbfbe38c1611f97affb1bf36fb2
lzpzx666 <2112568058@qq.com>
2023-10-05 11:09:33 +0800

    添加用户角色

---
+++ Person.java
@@ -0,0 +1,29 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:用户
+ *
+ * @Author: linZP
+ * DateTime: 2023-10-05 11:07
+ */
+public class Person {
+
+    String username;
+
+    String account;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+}
